b3ce3b350510a0d3402c406d103659bd85217527
Paolo Burgio <paolo.burgio@gmail.com>
2023-05-01 10:32:57 +0200

    Implemented timer with echo

--- Code/ros2/src/cpp_echo/src/client_member_function.cpp
+++ Code/ros2/src/cpp_echo/src/client_member_function.cpp
@@ -17,6 +17,9 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+using std::placeholders::_1;
+
+#include "rclcpp/clock.hpp"
 
 using namespace std::chrono_literals;
 
@@ -30,8 +33,14 @@ public:
   : Node("client"), count_(0)
   {
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+
+    subscription_ = this->create_subscription<std_msgs::msg::String>(
+      "echo", 10, std::bind(&Client::topic_callback, this, _1));
+
     timer_ = this->create_wall_timer(
-      500ms, std::bind(&Client::timer_callback, this));
+       500ms, std::bind(&Client::timer_callback, this));
+
+    clk_ = new rclcpp::Clock();
   }
 
 private:
@@ -40,11 +49,34 @@ private:
     auto message = std_msgs::msg::String();
     message.data = "Hello, world! " + std::to_string(count_++);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+
+    rclcpp::Time time = clk_->now();
+    start_ns_ = time.nanoseconds();
+
     publisher_->publish(message);
+
+    if(count_ >= NUM_MESSAGES)
+      this->timer_->cancel();
   }
+
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
   size_t count_;
+  const size_t NUM_MESSAGES = 10;
+
+  rclcpp::Clock *clk_;
+  rcl_time_point_value_t start_ns_;
+
+  void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
+  {
+    RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+
+    rcl_time_point_value_t end_ns = clk_->now().nanoseconds();
+    RCLCPP_INFO(this->get_logger(), "[__TIME__]: %lld", end_ns - start_ns_);
+    
+  }
+
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
 
 int main(int argc, char * argv[])
--- Code/ros2/src/cpp_echo/src/server_member_function.cpp
+++ Code/ros2/src/cpp_echo/src/server_member_function.cpp
@@ -24,6 +24,8 @@ public:
   Server()
   : Node("server")
   {
+    publisher_ = this->create_publisher<std_msgs::msg::String>("echo", 10);
+
     subscription_ = this->create_subscription<std_msgs::msg::String>(
       "topic", 10, std::bind(&Server::topic_callback, this, _1));
   }
@@ -32,7 +34,14 @@ private:
   void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
   {
     RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+
+    auto message = std_msgs::msg::String();
+    message.data = msg->data;
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    publisher_->publish(message);
   }
+
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
 
